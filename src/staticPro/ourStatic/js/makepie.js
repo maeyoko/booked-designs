090e545c877c4050711b234de80daadfcc1c5800
maeyoko <mae.yoko@yahoo.com>
2015-07-13 16:01:19 +0100

    pi fixed

--- src/staticPro/ourStatic/js/makepie.js
+++ src/staticPro/ourStatic/js/makepie.js
@@ -1,39 +1,39 @@
 AmCharts.makeChart("chartdiv",
-    {
-        "type": "pie",
-
-        "innerRadius": 10,
-        "labelRadius": 15,
-        "radius": 20,
-        "startAngle": 180,
-        "baseColor": "#009ED3",
-        "labelsEnabled": false,
-        "labelTickAlpha": 0,
-        "marginBottom": 0,
-        "marginTop": 0,
-        "outlineAlpha": 1,
-        "outlineThickness": 2,
-        "pullOutDuration": 0,
-        "sequencedAnimation": false,
-        "startDuration": 0,
-        "titleField": "category",
-        "urlTarget": "",
-        "valueField": "column-1",
-        "handDrawScatter": 0,
-        "panEventsEnabled": false,
-        "percentPrecision": 1,
-        "allLabels": [],
-        "balloon": {},
-        "titles": [],
-        "dataProvider": [
-            {
-                "category": "Available",
-                "column-1": "12"
-            },
-            {
-                "category": "In-Use",
-                "column-1": "4"
-            }
-        ]
-    }
+{
+	"type": "pie",
+	"balloonText": "",
+	"innerRadius": 10,
+	"labelRadius": 15,
+	"labelText": "",
+	"minRadius": 25,
+	"radius": 25,
+	"startAngle": 180,
+	"baseColor": "#009ED3",
+	"labelsEnabled": false,
+	"marginBottom": 0,
+	"marginTop": 0,
+	"outlineAlpha": 1,
+	"outlineThickness": 2,
+	"pullOutDuration": 0,
+	"sequencedAnimation": false,
+	"startDuration": 0,
+	"titleField": "country",
+	"valueField": "litres",
+	"fontSize": 12,
+	"panEventsEnabled": false,
+	"theme": "default",
+	"allLabels": [],
+	"balloon": {},
+	"titles": [],
+	"dataProvider": [
+		{
+			"country": "Czech Republic",
+			"litres": "75"
+		},
+		{
+			"country": "Belgium",
+			"litres": "25"
+		}
+	]
+}
 );
