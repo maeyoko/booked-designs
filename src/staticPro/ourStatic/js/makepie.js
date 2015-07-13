9e4748b4cea6e3c0ea2cbcb586c4ce6dc2a3b29e
maeyoko <mae.yoko@yahoo.com>
2015-07-13 11:09:35 +0100

    pie chart

---
+++ src/staticPro/ourStatic/js/makepie.js
@@ -0,0 +1,39 @@
+AmCharts.makeChart("chartdiv",
+    {
+        "type": "pie",
+
+        "innerRadius": 10,
+        "labelRadius": 15,
+        "radius": 20,
+        "startAngle": 180,
+        "baseColor": "#009ED3",
+        "labelsEnabled": false,
+        "labelTickAlpha": 0,
+        "marginBottom": 0,
+        "marginTop": 0,
+        "outlineAlpha": 1,
+        "outlineThickness": 2,
+        "pullOutDuration": 0,
+        "sequencedAnimation": false,
+        "startDuration": 0,
+        "titleField": "category",
+        "urlTarget": "",
+        "valueField": "column-1",
+        "handDrawScatter": 0,
+        "panEventsEnabled": false,
+        "percentPrecision": 1,
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "Available",
+                "column-1": "12"
+            },
+            {
+                "category": "In-Use",
+                "column-1": "4"
+            }
+        ]
+    }
+);
